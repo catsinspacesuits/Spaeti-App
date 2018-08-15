e5b04bfb3fb1d61243246f8a25e3d5dd8c59fdc3
Scott Munro <simunro@hotmail.co.uk>
2018-08-15 21:13:31 +0200

    commented out problematic migration

--- db/migrate/20180627115344_remove_toilet_available_from_stores.rb
+++ db/migrate/20180627115344_remove_toilet_available_from_stores.rb
@@ -1,5 +1,5 @@
 class RemoveToiletAvailableFromStores < ActiveRecord::Migration[5.1]
   def change
-    remove_column :stores, :toilet_available, :string
+    # remove_column :stores, :toilet_available, :string
   end
 end
